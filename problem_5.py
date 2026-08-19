bdaa55363e8225a5db36d174049c917aacaf063d
Deva5726 <sdevadharshini807@gmail.com>
2026-08-19 19:16:14 +0530

    Added problem_5.py

---
+++ problem_5.py
@@ -0,0 +1,16 @@
+# Prob 1:
+# Write a program which takes two values x and y. Prints x for y number of times.
+# Input:
+# 2 
+# 3
+# Expected Output
+# 2
+# 2
+# 2
+x=int(input())
+y=int(input())
+i=1
+print("Output:")
+while(i<=y):
+    print(x)
+    i+=1
