9685431921291351444ae62c1422df8e0283d600
Deva5726 <sdevadharshini807@gmail.com>
2026-08-19 19:24:12 +0530

    added problem_6.py

---
+++ problem_6.py
@@ -0,0 +1,20 @@
+# Write a program to take x and print multiples of x till 1000.
+# Input:
+# 100
+# Expected Output:
+# 100
+# 200
+# 300
+# 400
+# 500
+# 600
+# 700
+# 800
+# 900
+# 1000
+x=int(input())
+i=1
+print(" ")
+while(i<=10):
+    print(i*x)
+    i+=1
