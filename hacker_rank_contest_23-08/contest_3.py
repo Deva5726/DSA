ee4b5527b5d4208b52b3bacde99d3b8c135e6185
Deva5726 <sdevadharshini807@gmail.com>
2026-08-23 19:46:56 +0530

    added contest_3.py

---
+++ hacker_rank_contest_23-08/contest_3.py
@@ -0,0 +1,18 @@
+# Write a program which takes a value N and prints the sum of N natural numbers. Natural numbers start from 1.
+
+# Explanation: Sum of natural numbers from 1 to N, here N is 5, so 1+2+3+4+5 is 15. Input N = 5; Expected output : 15
+
+# Input Format
+
+# 5
+
+# Constraints
+
+# 1<= N <= 10^2
+
+# Output Format
+
+# 15
+N=int(input())
+total=N*(N+1)/2
+print(int(total))
