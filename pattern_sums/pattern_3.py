59fedb88b36606c62fd7a8f9294fd65e5ac02895
Deva5726 <sdevadharshini807@gmail.com>
2026-08-25 00:16:04 +0530

    added pattern_3.py

---
+++ pattern_sums/pattern_3.py
@@ -0,0 +1,15 @@
+# Input:
+# n= 6
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+print("input:")
+n=int(input("n = "))
+print("output:")
+print(" ")
+for i in range(1,n+1):
+    print("*"*i,end=" ")
+    print("\n")
